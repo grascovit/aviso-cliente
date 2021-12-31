6568749ff1ad30d41d043d08dfe6e5a76b703ac4
Gabriel Rascovit <rascovit@gmail.com>
2021-12-30 23:11:50 -0300

    enable sidekiq ui using http basic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
@@ -12,6 +14,17 @@ Rails.application.routes.draw do
       root to: 'devise/sessions#new', as: :unauthenticated_root
     end
   end
+
+  if Rails.env.production?
+    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username),
+                                                  ::Digest::SHA256.hexdigest(ENV['ADMIN_USERNAME'])) &
+        ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password),
+                                                    ::Digest::SHA256.hexdigest(ENV['ADMIN_PASSWORD']))
+    end
+  end
+
+  mount Sidekiq::Web, at: '/sidekiq'
 
   resources :appointments
   resources :clients
